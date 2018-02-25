d2b5974241394a967cb84dfc0ecc06f47c7ebbd5
David Asamu <davash001@gmail.com>
2018-02-26 00:20:09 +0100

    add about page + design

--- ifemun/conference/forms.py
+++ ifemun/conference/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Delegate
+from .models import Applicant
 
 class RegistrationForm(forms.ModelForm):
 
--- ifemun/conference/models.py
+++ ifemun/conference/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Delegate(models.Model):
+class Applicant(models.Model):
     # author = models.ForeignKey('settings.AUTH_USER_MODEL')
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
