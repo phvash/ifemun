9c016b395365360a14cb5ce48b23d8438ec140a6
David Asamu <davash001@gmail.com>
2018-03-02 07:35:40 +0100

    ssl certi

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,25 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from django.http import HttpResponse
+
+import csv
+
+
+def some_view(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['OBkEjbd81f7z5zLg9aa8TND4c0RTWP0xIfCREnnOMW8.qCtPt9nA3hRxpUcp0c_ob13Jy1cvZ9mWEt042rxdu94'])
+
+    return response
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/new_home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    url(r'^.well-known/acme-challenge/4KhjSv2R9210j0B4ILK8ZiIKbc9x3fJikUlRPE1pZOw$', some_view, name=''),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
@@ -38,3 +53,5 @@ if settings.DEBUG:
         urlpatterns = [
             url(r'^__debug__/', include(debug_toolbar.urls)),
         ] + urlpatterns
+
+
