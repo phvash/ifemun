2da56de4c1fe3634f86c7ebeaf5134f48002cd2f
phvash <davash001@gmail.com>
2018-05-16 20:18:19 +0100

    update to https

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,7 +13,7 @@ from .base import *  # noqa
 
 # DEBUG
 # ------------------------------------------------------------------------------
-DEBUG = env.bool('DJANGO_DEBUG', default=False)
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
 # SECRET CONFIGURATION
